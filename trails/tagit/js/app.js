ee863e67d6aca0d13a91fff6a2ade3ed571d0ec6
gokulakannanit <gokulakannanit@gmail.com>
2015-04-23 08:16:31 +0530

    Tags Added

--- trails/tagit/js/app.js
+++ trails/tagit/js/app.js
@@ -1,29 +1,20 @@
 angular.module("ngTagsInputSite", ['ngTagsInput'])    
 .controller('HomeCtrl', function($scope) {
-
     $scope.owner = 'Batman, Superman, Flash';
     $scope.tagList = ['Batman', 'Superman', 'Flash', 'The Punisher', 'Exam'];
 })
 .directive("ftTagInput", function(){
 
     function link(scope, elem, attr, ctrl){
-        scope.tags = scope.selectedTags.split(",");
+        scope.tagit = scope.selectedTags.split(",");
         scope.sourceList = scope.autoCompleteSource;
-
-        scope.$watch('tags', function(){
-            scope.selectedTags = scope.tags.join(",");
-            console.log(scope.myFormName);
-            ctrl.$setValidity(scope.myFormName, scope.tags.length>3);
+        scope.$watch('tagit', function(){
+            if(scope.tagit === 'undefined'){
+                scope.selectedTags = scope.tagit.join(",");
+                console.log("----",scope.minValue);
+                scope.myForm[scope.eleName].$setValidity((scope.tagit.length>=scope.minValue));
+            }            
         });
-
-        function isValid(value) {
-            console.log(scope.myFormName);
-            ctrl.$setValidity(scope.myFormName, (scope.tags.length>3));
-            return value;
-        }
-
-        ctrl.$parsers.unshift(isValid);
-        ctrl.$formatters.unshift(isValid);
     }
     return{
         restrict: 'AE',
@@ -31,9 +22,11 @@ angular.module("ngTagsInputSite", ['ngTagsInput'])
         scope:{
             selectedTags:'=ngModel',
             autoCompleteSource:'=',
-            myFormName:'=name'
+            eleName:'@name',
+            myForm:'=formname',
+            minValue:'@minValue'
         },
         link:link,
-        template:'<input type="hidden" name="myFormName" /> <tags-input ng-model="tags"><auto-complete source="sourceList"></auto-complete></tags-input>'
+        template:'<input type="hidden" name="eleName" /> <tags-input ng-model="tagit"><auto-complete source="sourceList"></auto-complete></tags-input>'
     }
 });
