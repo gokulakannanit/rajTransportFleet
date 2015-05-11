797bb72f64e656d937fb949ae77343a8a302e486
gokulakannanit <gokulakannanit@gmail.com>
2015-05-11 22:29:40 +0530

    Removed min length for phone and pan

--- js/app.js
+++ js/app.js
@@ -1,146 +1,159 @@
 var myAppDev = angular.module('mainModule', ['ui.router', 'ngTagsInput', 'ngAnimate']);
 myAppDev.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-    .state('home', {
-        url: '/home',
-        templateUrl: 'modules/dashboard',
-        controrller: 'dashboardCtrl'
-    })
-    .state('company', {
-        url: '/company',
-        templateUrl: 'modules/company',
-        controller:  'company.mainController'
-    })
-    .state('companyDetail', {
-        url: '/companyDetail/:isEdit/:editId',
-        templateUrl: 'modules/company/subModule/add',
-        controller:  'company.addController'
-    })
-    .state('owner', {
-        url: '/owner',
-        templateUrl: 'modules/owner',
-        controller:  'owner.mainController'
-    })
-    .state('ownerDetail', {
-        url: '/ownerDetail/:isEdit/:editId',
-        templateUrl: 'modules/owner/subModule/add',
-        controller:  'owner.addController'
-    })
-    .state('client', {
-        url: '/client',
-        templateUrl: 'modules/client',
-        controller:  'client.mainController'
-    })
-    .state('clientDetail', {
-        url: '/clientDetail/:isEdit/:editId',
-        templateUrl: 'modules/client/subModule/add',
-        controller:  'client.addController'
-    })
-    .state('vendor', {
-        url: '/vendor',
-        templateUrl: 'modules/vendor',
-        controller:  'vendor.mainController'
-    })
-    .state('vendorDetail', {
-        url: '/vendorDetail/:isEdit/:editId',
-        templateUrl: 'modules/vendor/subModule/add',
-        controller:  'vendor.addController'
-    })
-    .state('vehicle', {
-        url: '/vehicleInfo',
-        templateUrl: 'modules/vehicleInfo',
-        controller:  'vehicleInfo.mainController'
-    })
-    .state('vehicleDetail', {
-        url: '/vehicleDetail/:isEdit/:editId',
-        templateUrl: 'modules/vehicleInfo/subModule/add',
-        controller:  'vehicleInfo.addController'
-    })
-    .state('vehicleDetail.basic', {
-        views: {
-          url: '/basic',
-          "container": {
-            templateUrl: "modules/vehicleInfo/subModule/add/basic" 
-          }
-        }
-    })
-    .state('vehicleDetail.maintain', {
-        views: {
-          url: '/maintain',
-          "container": {
-            templateUrl: "modules/vehicleInfo/subModule/add/basic" 
-          }
-        }
-    })
-  $urlRouterProvider.otherwise('/home');
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: 'modules/dashboard',
+            controrller: 'dashboardCtrl'
+        })
+        .state('company', {
+            url: '/company',
+            templateUrl: 'modules/company',
+            controller: 'company.mainController'
+        })
+        .state('companyDetail', {
+            url: '/companyDetail/:isEdit/:editId',
+            templateUrl: 'modules/company/subModule/add',
+            controller: 'company.addController'
+        })
+        .state('owner', {
+            url: '/owner',
+            templateUrl: 'modules/owner',
+            controller: 'owner.mainController'
+        })
+        .state('ownerDetail', {
+            url: '/ownerDetail/:isEdit/:editId',
+            templateUrl: 'modules/owner/subModule/add',
+            controller: 'owner.addController'
+        })
+        .state('client', {
+            url: '/client',
+            templateUrl: 'modules/client',
+            controller: 'client.mainController'
+        })
+        .state('clientDetail', {
+            url: '/clientDetail/:isEdit/:editId',
+            templateUrl: 'modules/client/subModule/add',
+            controller: 'client.addController'
+        })
+        .state('vendor', {
+            url: '/vendor',
+            templateUrl: 'modules/vendor',
+            controller: 'vendor.mainController'
+        })
+        .state('vendorDetail', {
+            url: '/vendorDetail/:isEdit/:editId',
+            templateUrl: 'modules/vendor/subModule/add',
+            controller: 'vendor.addController'
+        })
+        .state('vehicle', {
+            url: '/vehicleInfo',
+            templateUrl: 'modules/vehicleInfo',
+            controller: 'vehicleInfo.mainController'
+        })
+        .state('vehicleDetail', {
+            url: '/vehicleDetail/:isEdit/:editId',
+            templateUrl: 'modules/vehicleInfo/subModule/add',
+            controller: 'vehicleInfo.addController'
+        })
+        .state('vehicleDetail.basic', {
+            views: {
+                url: '/basic',
+                "container": {
+                    templateUrl: "modules/vehicleInfo/subModule/add/basic"
+                }
+            }
+        })
+        .state('vehicleDetail.maintain', {
+            views: {
+                url: '/maintain',
+                "container": {
+                    templateUrl: "modules/vehicleInfo/subModule/add/basic"
+                }
+            }
+        })
+    $urlRouterProvider.otherwise('/home');
 
 });
 
-myAppDev.controller('dashboardCtrl', ['$rootScope', '$scope', function($rootScope, $scope){
-    $rootScope.activeMenu = 'dashboard';
-}]);
+myAppDev.controller('dashboardCtrl', ['$rootScope', '$scope',
+    function($rootScope, $scope) {
+        $rootScope.activeMenu = 'dashboard';
+    }
+]);
 
 myAppDev.factory('alertService', function($rootScope, $timeout) {
     var alertService = {};
 
-    function addAnimation(){
-        $timeout(function(){
-            angular.forEach($rootScope.alerts, function(item){
+    function addAnimation() {
+        $timeout(function() {
+            angular.forEach($rootScope.alerts, function(item) {
                 item.percent = 0;
             });
         }, 500);
-        $timeout(function(){
-            angular.forEach($rootScope.alerts, function(item, index){
-                item.animation='flipOutY';
+        $timeout(function() {
+            angular.forEach($rootScope.alerts, function(item, index) {
+                item.animation = 'flipOutY';
             });
         }, 3500);
-        $timeout(function(){
-            angular.forEach($rootScope.alerts, function(item, index){
+        $timeout(function() {
+            angular.forEach($rootScope.alerts, function(item, index) {
                 alertService.close(index);
             });
         }, 4000);
     }
 
     alertService.add = function(type, msg) {
-        var icons = {'success':'check', 'danger':'times-circle-o'};
-        $rootScope.alerts.push({'type': type, 'msg': msg, 'icon':icons[type], percent:100});
+        var icons = {
+            'success': 'check',
+            'danger': 'times-circle-o'
+        };
+        $rootScope.alerts.push({
+            'type': type,
+            'msg': msg,
+            'icon': icons[type],
+            percent: 100
+        });
         addAnimation();
     };
 
-    alertService.clear = function(){
+    alertService.clear = function() {
         $rootScope.alerts = [];
     }
 
-    alertService.close = function(index){
+    alertService.close = function(index) {
         $rootScope.alerts.splice(Number(index), 1);
-    }             
+    }
 
     return alertService;
 });
 
 myAppDev.factory('modalService', function($rootScope, $q, $timeout) {
-    var modalService = {}, defer,
-    buttons = {'delete':'danger'};
+    var modalService = {},
+        defer,
+        buttons = {
+            'delete': 'danger'
+        };
 
     modalService.show = function(obj) {
         defer = $q.defer();
         $rootScope.modalInitiated = true;
         $rootScope.ftModal = obj;
         $rootScope.ftModal.button = buttons[obj.action];
-        $rootScope.ftModal.show = true;        
+        $rootScope.ftModal.show = true;
         return defer.promise;
     };
 
-    $rootScope.$watch('ftModal.show', function(newVal){
-        if(defer && newVal === false){
+    $rootScope.$watch('ftModal.show', function(newVal) {
+        if (defer && newVal === false) {
             defer.resolve($rootScope.ftModal);
-        }        
+        }
     });
 
     return modalService;
 });
 
-myAppDev.directive("ftPanel", function () {
+myAppDev.directive("ftPanel", function() {
     return {
         restrict: "E",
         transclude: true,
@@ -148,9 +161,9 @@ myAppDev.directive("ftPanel", function () {
     };
 });
 
-myAppDev.directive("ftModal", function () {
+myAppDev.directive("ftModal", function() {
     function link(scope, element, attrs) {
-        scope.close = function(val){
+        scope.close = function(val) {
             scope.ftModal.confirmed = val;
             scope.ftModal.show = false;
         }
@@ -164,12 +177,12 @@ myAppDev.directive("ftModal", function () {
 
 });
 
-myAppDev.directive("ftAlert", function ($timeout, alertService) {
+myAppDev.directive("ftAlert", function($timeout, alertService) {
     function link(scope, element, attrs) {
         var selectedAlert;
-        scope.close = function (val){
+        scope.close = function(val) {
             selectedAlert = val;
-            $timeout(function(){
+            $timeout(function() {
                 alertService.close(selectedAlert);
             }, 500);
         }
@@ -183,99 +196,106 @@ myAppDev.directive("ftAlert", function ($timeout, alertService) {
 
 });
 
-myAppDev.directive("ftTagInput", function(){
+myAppDev.directive("ftTagInput", function() {
 
-    function link(scope, elem, attr, ctrl){
+    function link(scope, elem, attr, ctrl) {
         scope.tagit = [];
-        if(scope.selectedTags !== ''){
+        if (scope.selectedTags !== '') {
             scope.tagit = scope.selectedTags.split(",");
-        }        
+        }
         scope.sourceList = scope.autoCompleteSource;
-        scope.onTagAdded = function(){
+        scope.onTagAdded = function() {
             scope.selectedTags = scope.tagit.join(",");
-            ctrl.$setValidity(scope.eleName, (scope.tagit.length>=scope.minValue));
+            ctrl.$setValidity(scope.eleName, (scope.tagit.length >= scope.minValue));
         }
     }
-    return{
+    return {
         restrict: 'AE',
         require: 'ngModel',
-        scope:{
-            selectedTags:'=ngModel',
-            autoCompleteSource:'=',
-            eleName:'@name',
-            minValue:'@minValue'
+        scope: {
+            selectedTags: '=ngModel',
+            autoCompleteSource: '=',
+            eleName: '@name',
+            minValue: '@minValue'
         },
-        link:link,
-        template:'<tags-input ng-model="tagit" on-tag-added="onTagAdded();" on-tag-removed="onTagAdded();"><auto-complete source="sourceList"></auto-complete></tags-input>'
+        link: link,
+        template: '<tags-input ng-model="tagit" on-tag-added="onTagAdded();" on-tag-removed="onTagAdded();"><auto-complete source="sourceList"></auto-complete></tags-input>'
     }
 });
 
-myAppDev.controller("appController", function($rootScope){
-     // create an array of alerts available globally
+myAppDev.controller("appController", function($rootScope) {
+    // create an array of alerts available globally
     $rootScope.alerts = [];
     $rootScope.ftModal = {};
 });
 
 myAppDev.filter('titleCase', function() {
     return function(input) {
-      input = input || '';
-      return input.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+        input = input || '';
+        return input.replace(/\w\S*/g, function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        });
     };
 })
 
-myAppDev.directive("ftFormText", function($compile){
-    function link(scope, elem, attr, ctrl){
+myAppDev.directive("ftFormText", function($compile) {
+    function link(scope, elem, attr, ctrl) {
         scope.opts = attr;
-        if(!scope.opts.type){
+        if (!scope.opts.type) {
             scope.opts.type = "text";
         }
         scope.form = ctrl;
 
         $compile(elem.contents())(scope);
-        
-        function validateEmail(email) {
-            var expr = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-            return expr.test(email);
-        }
-        function validateMinLength(val){
-            return !(val.length >= scope.opts.minlen);
-        }
-        function validateMaxLength(val){
-            return !(val.length <= scope.opts.maxlen);
-        }
+
         var input = $(elem).find("input");
 
-        if(scope.opts.required){
+        if (scope.opts.required) {
             input.attr('required', 'true');
         }
 
-        input.on("blur keyup", function(e){
-            
-            if($(this).prop('type') == "email"){
-                ctrl.$setValidity('inValid', validateEmail($(this).val()));
+        ctrl.$validators.minlen = function(modelValue, viewValue) {
+            if (scope.opts.minlen && viewValue !== '') {
+                return (viewValue.length >= scope.opts.minlen);
             }
+            return true;
+        }
 
-            if(scope.opts.minlen){
-                ctrl.$setValidity('inValid', validateMinLength($(this).val()));
+        ctrl.$validators.maxlen = function(modelValue, viewValue) {
+            if (scope.opts.maxlen && viewValue !== '') {
+                return (viewValue.length <= scope.opts.maxlen);
             }
+            return true;
+        }
 
-            if(scope.opts.maxlen){
-                ctrl.$setValidity('inValid', validateMaxLength($(this).val()));
+        ctrl.$validators.pattern = function(modelValue, viewValue) {
+            if (scope.opts.pattern && viewValue !== '') {
+                var pattern = new RegExp(scope.opts.pattern);
+                return (pattern).test(viewValue);
             }
+            return true;
+        }
 
-            if($(this).prop('required') || $(this).val() !== ''){
-                ctrl.$setTouched(true);
+        ctrl.$validators.email = function(modelValue, viewValue) {
+            if (scope.opts.type === 'email' && viewValue !== '') {
+                var expr = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+                return expr.test(viewValue);
             }
-            
+            return true;
+        }
+
+        input.on("blur keydown", function(e) {
+            ctrl.$setTouched(true);
         });
+
     }
-    return{
+    return {
         restrict: 'AE',
         require: 'ngModel',
-        scope:{
-            model:'=ngModel',
+        scope: {
+            model: '=ngModel',
         },
-        link:link,
-        templateUrl:'partials/formControl.html'
+        link: link,
+        templateUrl: 'partials/formControl.html'
     }
 });
